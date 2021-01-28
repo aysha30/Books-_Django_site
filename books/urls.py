56ad482417d023786964589d3441f9df383694a3
Aysha Yazdani <ettashamriya@gmail.com>
2021-01-28 22:09:29 +0530

    First commit

---
+++ books/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.conf.urls import url
+from . import views
+
+app_name = 'books'
+
+urlpatterns = [
+   url(r'^$', views.IndexView.as_view(), name='index'),
+   #/books/2/
+   url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+   url(r'books/add/$', views.BookCreate.as_view(), name='book-add'),
+]
